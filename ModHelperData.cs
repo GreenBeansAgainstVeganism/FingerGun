fcc8e6c76e93ccc2c7cce423d4cbb5f68be5a907
GreenBeansAgainstVeganism <kradrling@gmail.com>
2023-05-19 11:39:35 -0700

    set up metadata

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,11 +4,12 @@ namespace FingerGun
   {
     public const string WorksOnVersion = "36.3";
     public const string Version = "1.0.0";
-    public const string Name = "FingerGun";
+    public const string Name = "Finger Gun";
 
     public const string Description = "Lets you use your mouse button as a weapon.";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "GreenBeansAgainstVeganism"; 
+    public const string RepoName = "FingerGun"; 
+    public const string Author = "AttackDuck";
   }
 }
